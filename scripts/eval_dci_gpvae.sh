fc9cfc8b4dcadd2821d5609db818ad568e517f6c
simonbing <bings@student.ethz.ch>
2020-04-13 18:07:01 +0200

    added job group

--- scripts/eval_dci_gpvae.sh
+++ scripts/eval_dci_gpvae.sh
@@ -4,7 +4,7 @@
 
 for dim in 8; do
   for base_dir in models/dsprites_dim_$dim/; do
-    bsub -R "rusage[mem=16000]" python dsprites_dci.py --z_name factors_5000.npy \
+    bsub -g /gpvae_norm -R "rusage[mem=16000]" python dsprites_dci.py --z_name factors_5000.npy \
     --model_name /dsprites_dim_$dim/$base_dir
   done
 done
