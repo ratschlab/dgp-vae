d64b0db1a18fe23cb275cb50bf9e3e6a724edc75
simonbing <bings@student.ethz.ch>
2020-04-13 12:37:57 +0200

    try by training on GTX explicitly

--- scripts/train_gpvae.sh
+++ scripts/train_gpvae.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create directory for each tested latent dimension
-for dim in 8 16 32 64 128 256; do
-  mkdir -p models/dsprites_dim_$dim
-done
+# for dim in 8 16 32 64 128 256; do
+#   mkdir -p models/dsprites_dim_$dim
+# done
 
 for n in {3..10}; do
   SEED=$RANDOM
   for dim in 8 16 32 64 128; do
 
-  bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
+  bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
   python train.py --model_type gp-vae --data_type dsprites --exp_name dsprites_"$dim"_n$n \
   --basedir models/dsprites_dim_"$dim" --seed $SEED --banded_covar --latent_dim $dim \
   --encoder_sizes=32,256,256 --decoder_sizes=256,256,256 --window_size 3 --sigma 1 \
