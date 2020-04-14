ae4eb047afdc9b87c8a5572ccd7878b34a1a9176
simonbing <bings@student.ethz.ch>
2020-04-14 11:52:53 +0200

    debug5

--- dci_aggregate.py
+++ dci_aggregate.py
@@ -29,6 +29,8 @@ def aggregate(N, latent_dims, base_dir='dsprites_dim_'):
         for _, dirs, _ in os.walk(os.path.join("models", dim_dir)):
             for dir in dirs:
                 print(dir)
+                dci_file = glob(os.path.join(dir,'dci*'))
+                print(dci_file)
                 for _, _, files in os.walk(os.path.abspath(dir)):
                     print('FILES: ',files)
                     for filename in files:
