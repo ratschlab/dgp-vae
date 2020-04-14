f63bffbbb0c0241b724158f173255385a7c93016
simonbing <bings@student.ethz.ch>
2020-04-14 17:59:15 +0200

    first commit for aggregation script

--- dci_aggregate.py
+++ dci_aggregate.py
@@ -8,6 +8,7 @@ ETHZ 2020
 import numpy as np
 import os
 from glob import glob
+import json
 from absl import flags, app
 
 FLAGS = flags.FLAGS
@@ -44,6 +45,30 @@ def aggregate_gpvae(N, latent_dims, base_dir='dsprites_dim_'):
 
     return dci_scores
 
+def aggregate_baseline(N, base_dir='dim_64'):
+    """
+    Collects all dci scores and aggregates into single array.
+    Args:
+        N, int:             Number of random seeds
+        base_dir, string:   Base directory naming scheme
+
+    Returns:
+        dci_scores, [3xN] np array
+    """
+    dci_scores = np.zeros((3,N), dtype=np.float32)
+
+    base_path = os.path.join('baselines', FLAGS.model, base_dir)
+
+    for _, dirs, _ in os.walk(base_path):
+        for n, dir in enumerate(dirs):
+            dci = json.load(os.path.join(base_path, dir, 'metrics', 'dci', 'results',
+                                      'aggregate', 'evaluation.json')) # PROPERLY PARSE JSON FILE
+            dci_scores[0,n] = dci['evaluation_results.disentanglement']
+            dci_scores[1,n] = dci['evaluation_results.completeness']
+            dci_scores[2,n] = dci['evaluation_results.informativeness_test']
+
+    return dci_scores
+
 def main(argv):
     del argv # Unused
 
@@ -53,7 +78,7 @@ def main(argv):
         latent_dims = [8, 16, 32, 64, 128]
         dci_scores = aggregate_gpvae(n_experiments, latent_dims)
     elif FLAGS.model in ['betatcvae', 'factorvae', 'dipvae_i']:
-        blabla
+        dci_scores = aggregate_baseline(n_experiments)
     else:
         raise ValueError("Model must be one of: ['gpvae', 'betatcvae', 'factorvae', 'dipvae_i']")
 
