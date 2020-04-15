878253ba6766406e993f14d7db06438a7bde261b
simonbing <bings@student.ethz.ch>
2020-04-15 21:05:28 +0200

    debugging for lower DCI scores. try with from_tensor_slices

---
+++ scripts/train_gpvae_debug.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create directory for each tested latent dimension
+# for dim in 8 16 32 64 128 256; do
+#   mkdir -p models/dsprites_dim_$dim
+# done
+
+for n in 1; do
+  SEED=$RANDOM
+  for dim in 8 16 32 64 128; do
+
+  bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
+  python train.py --model_type gp-vae --data_type dsprites --exp_name dsprites_"$dim"_debug_n$n \
+  --basedir models/dsprites_dim_"$dim"_debug --seed $SEED --banded_covar --latent_dim $dim \
+  --encoder_sizes=32,256,256 --decoder_sizes=256,256,256 --window_size 3 --sigma 1 \
+  --length_scale 2 --beta 1.0 --num_epochs 20
+
+  done
+done
