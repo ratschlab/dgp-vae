9a49cb570a1cdd59f6603aa79ae22e5277f45267
simonbing <bings@student.ethz.ch>
2020-04-16 16:24:53 +0200

    fixed seed and output test

--- scripts/train_gpvae_debug.sh
+++ scripts/train_gpvae_debug.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Create directory for each tested latent dimension
-for dim in 8 16 32 64 128 256; do
+for dim in 8 16 32 64 128; do
   mkdir -p models/dsprites_dim_"$dim"_debug
 done
 
 for n in 1; do
-  SEED=$RANDOM
-  for dim in 8 16 32 64 128; do
+  SEED=4731
+  for dim in 8 16; do
 
-  bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
+  bsub -o models/test_out_$dim -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
   python train.py --model_type gp-vae --data_type dsprites --exp_name dsprites_"$dim"_debug_n$n \
-  --basedir models/dsprites_dim_"$dim"_debug --seed $SEED --banded_covar --latent_dim $dim \
+  --basedir models/dsprites_dim_"$dim"_debug2 --seed $SEED --banded_covar --latent_dim $dim \
   --encoder_sizes=32,256,256 --decoder_sizes=256,256,256 --window_size 3 --sigma 1 \
   --length_scale 2 --beta 1.0 --num_epochs 20
 
