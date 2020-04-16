05effcdd11bb00c7f41f84530c91fd13c231e4e5
simonbing <bings@student.ethz.ch>
2020-04-16 19:18:29 +0200

    debug script for good seed

--- scripts/train_gpvae_debug.sh
+++ scripts/train_gpvae_debug.sh
@@ -9,7 +9,7 @@ for n in 1; do
   SEED=4731
   for dim in 8 16 32 64 128; do
 
-  bsub -o models/dsprites_dim_"$dim"_debug2/lfs_log_$dim -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
+  bsub -o models/dsprites_dim_"$dim"_debug2/lfs_log_"$dim"_$n -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
   python train.py --model_type gp-vae --data_type dsprites --exp_name dsprites_"$dim"_debug_n$n \
   --basedir models/dsprites_dim_"$dim"_debug2 --seed $SEED --banded_covar --latent_dim $dim \
   --encoder_sizes=32,256,256 --decoder_sizes=256,256,256 --window_size 3 --sigma 1 \
