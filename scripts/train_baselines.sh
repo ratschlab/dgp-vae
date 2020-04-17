540dbd89822552df95458817c32a5f4a71357a7e
simonbing <bings@student.ethz.ch>
2020-04-17 09:39:00 +0200

    output debug

--- scripts/train_baselines.sh
+++ scripts/train_baselines.sh
@@ -3,9 +3,9 @@
 # Train all baselines
 for n in {1..10}; do
   SEED=$RANDOM
-  bsub -o baselines/betatcvae/betatcvae_n"$n"/lsf_log_$n -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
+  bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
   baselines/betatcvae/train.py --output_dir betatcvae_n$n --seed $SEED
-  bsub -o baselines/betatcvae/factorvae_n"$n"/lsf_log_$n -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
+  bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
   baselines/factorvae/train.py --output_dir factorvae_n$n --seed $SEED
   # bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
   # baselines/dipvae_I/train.py --output_dir dipvae_I_n$n --seed $SEED
