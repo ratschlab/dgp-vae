13e190ededfef64383f136595c691aa75f47093c
simonbing <bings@student.ethz.ch>
2020-04-17 09:44:08 +0200

    corrected latent dim for baselines

--- scripts/train_baselines.sh
+++ scripts/train_baselines.sh
@@ -4,9 +4,9 @@
 for n in {1..10}; do
   SEED=$RANDOM
   bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
-  baselines/betatcvae/train.py --output_dir betatcvae_n$n --seed $SEED
+  python baselines/betatcvae/train.py --output_dir betatcvae_n$n --seed $SEED
   bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
-  baselines/factorvae/train.py --output_dir factorvae_n$n --seed $SEED
+  python baselines/factorvae/train.py --output_dir factorvae_n$n --seed $SEED
   # bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
-  # baselines/dipvae_I/train.py --output_dir dipvae_I_n$n --seed $SEED
+  # python baselines/dipvae_I/train.py --output_dir dipvae_I_n$n --seed $SEED
 done
