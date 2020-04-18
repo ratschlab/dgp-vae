24b8bec4b30f0b3e824666ff085c28eb0fac042a
simonbing <bings@student.ethz.ch>
2020-04-18 15:10:30 +0200

    modified training script for gp sampled dataset

--- scripts/train_gpvae.sh
+++ scripts/train_gpvae.sh
@@ -2,16 +2,16 @@
 
 # Create directory for each tested latent dimension
 for dim in 8 16 32 64 128; do
-  mkdir -p models/dsprites_dim_"$dim"_tensor2
+  mkdir -p models/dsprites_dim_"$dim"_gp1
 done
 
 for n in {1..10}; do
   SEED=$RANDOM
   for dim in 8 16 32 64 128; do
 
-  bsub -o models/dsprites_dim_"$dim"_tensor2/lfs_log_"$dim"_$n -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
+  bsub -o models/dsprites_dim_"$dim"_gp1/lfs_log_"$dim"_$n -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
   python train.py --model_type gp-vae --data_type dsprites --exp_name dsprites_"$dim"_n$n \
-  --basedir models/dsprites_dim_"$dim"_tensor2 --seed $SEED --banded_covar --latent_dim $dim \
+  --basedir models/dsprites_dim_"$dim"_gp1 --seed $SEED --banded_covar --latent_dim $dim \
   --encoder_sizes=32,256,256 --decoder_sizes=256,256,256 --window_size 3 --sigma 1 \
   --length_scale 2 --beta 1.0 --num_epochs 20
 
