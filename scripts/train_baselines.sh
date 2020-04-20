0a0d142db421f3d592c6ade82bfc94198f2dc8ac
simonbing <bings@student.ethz.ch>
2020-04-20 11:56:07 +0200

    added baseline script

--- scripts/train_baselines.sh
+++ scripts/train_baselines.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Train all baselines
-for n in 7; do
+for n in {1..10}; do
   SEED=$RANDOM
-  bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
-  python baselines/betatcvae/train.py --output_dir betatcvae_n$n --seed $SEED
-  bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
-  python baselines/factorvae/train.py --output_dir factorvae_n$n --seed $SEED
   # bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
-  # python baselines/dipvae_I/train.py --output_dir dipvae_I_n$n --seed $SEED
+  # python baselines/betatcvae/train.py --output_dir betatcvae_n$n --seed $SEED
+  # bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
+  # python baselines/factorvae/train.py --output_dir factorvae_n$n --seed $SEED
+  bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
+  python baselines/dipvae_i/train.py --output_dir dipvae_i_n$n --seed $SEED
 done
