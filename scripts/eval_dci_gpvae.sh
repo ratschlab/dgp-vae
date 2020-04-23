aaa8d71418ca1117fe708bdde99cfc50a7698008
simonbing <bings@student.ethz.ch>
2020-04-23 08:58:30 +0200

    typo

--- scripts/eval_dci_gpvae.sh
+++ scripts/eval_dci_gpvae.sh
@@ -3,7 +3,7 @@
 # Evaluate DCI metric for gpvae models
 
 for dim in 8 16 32 64 128; do
-  for base_dir in models/dsprites_dim_"$dim"_sind_rand1/*; do
+  for base_dir in models/dsprites_dim_"$dim"_sin_rand1/*; do
     bsub -g /gpvae_norm -R "rusage[mem=16000]" python dsprites_dci.py --z_name factors_sin_rand_5000.npy \
     --model_name $base_dir
   done
