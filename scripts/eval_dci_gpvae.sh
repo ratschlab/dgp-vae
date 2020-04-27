17a01fb3c60d00f59857c0dfbf6bbccef1e9b2f4
simonbing <bings@student.ethz.ch>
2020-04-27 09:26:40 +0200

    dco eval sin no ss

--- scripts/eval_dci_gpvae.sh
+++ scripts/eval_dci_gpvae.sh
@@ -3,8 +3,8 @@
 # Evaluate DCI metric for gpvae models
 
 for dim in 8 16 32 64 128; do
-  for base_dir in models/dsprites_dim_"$dim"_sin_order_ss1/*; do
-    bsub -g /gpvae_norm -R "rusage[mem=16000]" python dsprites_dci.py --z_name factors_sin_order_ss_5000.npy \
+  for base_dir in models/dsprites_dim_"$dim"_sin_no_ss1/*; do
+    bsub -g /gpvae_norm -R "rusage[mem=16000]" python dsprites_dci.py --z_name factors_sin_no_ss_5000.npy \
     --model_name $base_dir
   done
 done
