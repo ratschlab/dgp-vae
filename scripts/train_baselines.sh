8d46c4b52b5c9c332dbcae45934a4946c48ee9ac
simonbing <bings@student.ethz.ch>
2020-04-28 13:04:45 +0200

    redo baseline experiments fact dip

--- scripts/train_baselines.sh
+++ scripts/train_baselines.sh
@@ -3,9 +3,9 @@
 # Train all baselines
 for n in {1..10}; do
   SEED=$RANDOM
-  bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
-  python baselines/betatcvae/train.py --output_dir betatcvae_n$n --seed $SEED \
-  --dim 64 --subset sin
+  # bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
+  # python baselines/betatcvae/train.py --output_dir betatcvae_n$n --seed $SEED \
+  # --dim 64 --subset sin
   bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
   python baselines/factorvae/train.py --output_dir factorvae_n$n --seed $SEED \
   --dim 64 --subset sin
