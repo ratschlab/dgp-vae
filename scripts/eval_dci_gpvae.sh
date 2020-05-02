fad474aae451b19c8b277e55f533c0900f5cefdf
simonbing <bings@student.ethz.ch>
2020-05-02 20:37:45 +0200

    10 epoch eval

--- scripts/eval_dci_gpvae.sh
+++ scripts/eval_dci_gpvae.sh
@@ -3,7 +3,7 @@
 # Evaluate DCI metric for gpvae models
 
 for dim in 64; do
-  for base_dir in models/dim/dsprites_dim_"$dim"_0/*; do
+  for base_dir in models/dim/dsprites_dim_"$dim"_1/*; do
     bsub -g /gpvae_norm -R "rusage[mem=16000]" python dsprites_dci.py --z_name factors_dsprites_100k_5k.npy \
     --model_name $base_dir
   done
