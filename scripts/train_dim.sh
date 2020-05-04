da1538d678abe0f76e4c6824b2b798a88f5452b3
simonbing <bings@student.ethz.ch>
2020-05-04 10:33:18 +0200

    forgot testing

--- scripts/train_dim.sh
+++ scripts/train_dim.sh
@@ -9,12 +9,13 @@ for n in {1..10}; do
   SEED=$RANDOM
   for dim in 64; do
 
-  bsub -o models/dim/dsprites_dim_"$dim"_1/log_"$dim"_$n -g /gpvae_disent -R "rusage[mem=120000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
-  python train_exp.py --model_type gp-vae --data_type dsprites \
+  bsub -o models/dim/dsprites_dim_"$dim"_1/log_"$dim"_$n -g /gpvae_disent \
+  -R "rusage[mem=120000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
+  python train_exp.py --model_type gp-vae --data_type dsprites --testing\
   --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_100k_5k.npz \
-  --exp_name dsprites_"$dim"_n$n --basedir models/dim/dsprites_dim_"$dim"_1 --seed $SEED --banded_covar --latent_dim $dim \
-  --encoder_sizes=32,256,256 --decoder_sizes=256,256,256 --window_size 3 --sigma 1 \
-  --length_scale 2 --beta 1.0 --num_epochs 10
+  --exp_name dsprites_"$dim"_n$n --basedir models/dim/dsprites_dim_"$dim"_1 \
+  --seed $SEED --banded_covar --latent_dim $dim --encoder_sizes=32,256,256 \
+  --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 --num_epochs 1
 
   done
 done
