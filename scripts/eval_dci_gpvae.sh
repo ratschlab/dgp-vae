ed4eb05664011eccf684d54a8bfed9c8caa6a2e2
simonbing <bings@student.ethz.ch>
2020-05-07 14:52:56 +0200

    dci eval len scales

--- scripts/eval_dci_gpvae.sh
+++ scripts/eval_dci_gpvae.sh
@@ -2,8 +2,8 @@
 
 # Evaluate DCI metric for gpvae models
 
-for dim in 64; do
-  for base_dir in models/dim/dsprites_dim_"$dim"_2/*; do
+for len in 1e-1 5e-1 1 2 4 8; do
+  for base_dir in models/len/dsprites_len_"$len"_1/*; do
     bsub -g /gpvae_norm -R "rusage[mem=16000]" python dsprites_dci.py --z_name factors_100k_5k.npz \
     --model_name "$base_dir"
   done
