8580a24d8287a2dbc8fe9615bceb5a85141dba16
simonbing <bings@student.ethz.ch>
2020-05-08 10:39:36 +0200

    debugging dci eval script

--- scripts/eval_dci_gpvae.sh
+++ scripts/eval_dci_gpvae.sh
@@ -4,7 +4,9 @@
 
 for len in 1e-1 5e-1 1 2 4 8; do
   for base_dir in models/len/dsprites_len_"$len"_1/*; do
-    bsub -g /gpvae_norm -R "rusage[mem=16000]" python dsprites_dci.py --z_name factors_100k_5k.npz \
-    --model_name "$base_dir"
+#    bsub -g /gpvae_norm -R "rusage[mem=16000]" python dsprites_dci.py --z_name factors_100k_5k.npz \
+#    --model_name "$base_dir"
+     echo AHHHHHHHHHHHH
+     echo $"base_dir"
   done
 done
