71d39f1633310be4445cf983ff22ef202e26f56b
simonbing <bings@student.ethz.ch>
2020-05-11 18:12:21 +0200

    Adds baseline train steps experiment

--- scripts/baselines_steps_exp.sh
+++ scripts/baselines_steps_exp.sh
@@ -12,9 +12,14 @@
 #  done
 #done
 
-for model in betatcvae factorvae dip_vae_i; do
-  echo -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
-  python baselines/train_baseline.py --base_dir exp_steps0 \
-  --output_dir steps_"$steps"_n_"$n" --subset sin_rand --dim 64\
-  --model "$model" --seed "$seed" --steps "$steps"
+for steps in 15620 62480; do
+  for n in 5; do
+    seed=$RANDOM
+    for model in betatcvae factorvae dip_vae_i; do
+      echo -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
+      python baselines/train_baseline.py --base_dir exp_steps0 \
+      --output_dir steps_"$steps"_n_"$n" --subset sin_rand --dim 64\
+      --model "$model" --seed "$seed" --steps "$steps"
+    done
+  done
 done
