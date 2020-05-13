2951b0bc9e9770ae5e68cea41c6691522371a83e
simonbing <bings@student.ethz.ch>
2020-05-13 10:53:18 +0200

    updates steps experiment

--- scripts/baselines_steps_exp.sh
+++ scripts/baselines_steps_exp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for steps in 15620 62480 93720 124960 156200; do
+for steps in 15620 50000 100000 200000 300000; do
   for n in {1..10}; do
     seed=$RANDOM
-    for model in betatcvae factorvae dipvae_i; do
+    for model in annealedvae betavae betatcvae factorvae dipvae_i dipvae_ii; do
       bsub -g /gpvae_disent -R "rusage[mem=16000,ngpus_excl_p=1]" \
-      python baselines/train_baseline.py --base_dir exp_steps0 \
+      python baselines/train_baseline.py --base_dir exp_steps1/steps_"$steps" \
       --output_dir steps_"$steps"_n_"$n" --subset sin_rand --dim 64\
       --model "$model" --seed "$seed" --steps "$steps"
     done
