5bef05652cc049647dcb76a09a4d71152f500bfe
simonbing <bings@student.ethz.ch>
2020-06-25 18:21:46 +0200

    Extend runtime

--- scripts/gpvae_steps_subset.sh
+++ scripts/gpvae_steps_subset.sh
@@ -9,7 +9,7 @@ seeds=(20546 15386 3698 6891 21971 10675 19213 27236 24241 15150)
 for n in ${!seeds[@]}; do
   let n+=1
   for epochs in 14 19; do
-    bsub -o models/gpvae/epoch_sweep0/epoch_"$epochs"/log_"$epochs"_"$n" -g /gpvae_disent \
+    bsub -o models/gpvae/epoch_sweep0/epoch_"$epochs"/log_"$epochs"_"$n" -g /gpvae_disent -W 7:00 \
     -R "rusage[mem=200000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]"\
     python run_experiment.py --model_type gp-vae --data_type dsprites --testing\
     --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_100k_5k.npz \
