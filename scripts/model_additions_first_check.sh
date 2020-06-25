2544efd12ca58375f1b24fab6416311626c872d7
simonbing <bings@student.ethz.ch>
2020-06-25 15:09:02 +0200

    Updates flag in script

--- scripts/model_additions_first_check.sh
+++ scripts/model_additions_first_check.sh
@@ -23,7 +23,7 @@ for n in {1..10}; do
   --model_type gp-vae --data_type dsprites --testing \
   --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_100k_5k.npz \
   --exp_name init_test_"$n" --basedir models/lagging_inf/test_dump --seed "$seed" --banded_covar \
-  --latent_dim 64 --encoder_sizes=32,256,256 --decoder_sizes=256,256,256 --lagging_inference \
+  --latent_dim 64 --encoder_sizes=32,256,256 --decoder_sizes=256,256,256 --aggressive_train \
   --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 --num_epochs 2 --kernel cauchy \
   --print_interval 500 --z_name factors_100k_5k.npz --save_score
 done
