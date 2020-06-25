a55c5a37159b7ed57b55832311e8a7069e882858
simonbing <bings@student.ethz.ch>
2020-06-25 10:58:11 +0200

    add comment

--- scripts/baselines_steps_missing_full.sh
+++ scripts/baselines_steps_missing_full.sh
@@ -4,8 +4,8 @@
 seeds=(3963 25513 10353 14012 19903 26616 32096 22381 23778 10513)
 for n in ${!seeds[@]}; do
   let n+=1
-  echo "$n"
-  echo "${seeds[$n-1]}"
+#  echo "$n"
+#  echo "${seeds[$n-1]}"
   for steps in 200000 300000; do
     bsub -W 13:00 -g /disent_baseline_24 -R "rusage[mem=16000,ngpus_excl_p=1]" \
     python baselines/train_baseline.py --base_dir exp_steps_full1/steps_"$steps" \
