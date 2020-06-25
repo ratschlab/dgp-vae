e535d45f12dfb7c2e7a6e078a91d019555139e67
simonbing <bings@student.ethz.ch>
2020-06-25 19:09:13 +0200

    Lagging inference test script

--- scripts/model_additions_first_check.sh
+++ scripts/model_additions_first_check.sh
@@ -22,7 +22,7 @@ for n in {1..10}; do
   -R "select[gpu_model0==GeForceGTX1080Ti]" python run_experiment.py \
   --model_type gp-vae --data_type dsprites --testing \
   --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_100k_5k.npz \
-  --exp_name init_test_"$n" --basedir models/lagging_inf/test_dump --seed "$seed" --banded_covar \
+  --exp_name init_test_"$n" --basedir models/lagging_inf1/test_dump --seed "$seed" --banded_covar \
   --latent_dim 64 --encoder_sizes=32,256,256 --decoder_sizes=256,256,256 --aggressive_train \
   --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 --num_epochs 2 --kernel cauchy \
   --print_interval 500 --z_name factors_100k_5k.npz --save_score
