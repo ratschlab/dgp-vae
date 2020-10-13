860c3f95e73e30c876c911b98a702fc18e348896
simonbing <bings@student.ethz.ch>
2020-10-13 13:55:36 +0200

    Fixed naming of factors and codes

--- dsprites_dci.py
+++ dsprites_dci.py
@@ -18,24 +18,22 @@ import os
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('z_path', '/cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_5000.npy', 'File path for underlying factors z')
+flags.DEFINE_string('c_path', '/cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_5000.npy', 'File path for underlying factors z')
 flags.DEFINE_string('model_name', '', 'Name of model directory to get learned latent code')
 flags.DEFINE_bool('save_score', False, 'Whether or not to save calculated score')
 
-def load_z_c(z_path, c_path):
-    # z_test = np.load(z_path)
-
+def load_z_c(c_path, z_path):
     try:
-        z_full = np.load(z_path)['factors_test']
+        c_full = np.load(c_path)['factors_test']
     except IndexError:
-        z_full = np.load(z_path)
-    c = np.load(c_path)
+        c_full = np.load(c_path)
+    z = np.load(z_path)
 
     # Check length of c and only take same amount of z values. Corresponds to z_test.
-    z = z_full[:c.shape[0],:,:]
+    c = c_full[:z.shape[0],:,:]
     assert z.shape[0] == c.shape[0]
 
-    return z, c
+    return c, z
 
 def main(argv, model_dir=None):
     del argv # Unused
@@ -45,11 +43,11 @@ def main(argv, model_dir=None):
     else:
         out_dir = model_dir
 
-    c_path = '{}/z_mean.npy'.format(out_dir)
+    z_path = '{}/z_mean.npy'.format(out_dir)
     # project_path = '/cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_5000.npy'
     # z_path = os.path.join(project_path, FLAGS.z_name)
 
-    z, c = load_z_c(FLAGS.z_path, c_path)
+    c, z = load_z_c(FLAGS.c_path, z_path)
 
     z_shape = z.shape
     c_shape = c.shape
@@ -58,15 +56,15 @@ def main(argv, model_dir=None):
     c_reshape = np.reshape(np.transpose(c, (0,2,1)),(c_shape[0]*c_shape[2],c_shape[1]))
 
     # Check if latent factor doesn't change and remove if is the case
-    mask = np.ones(z_reshape.shape[1], dtype=bool)
-    for i in range(z_reshape.shape[1]):
-        z_change = np.sum(np.diff(z_reshape[:,i]))
-        if not z_change:
+    mask = np.ones(c_reshape.shape[1], dtype=bool)
+    for i in range(c_reshape.shape[1]):
+        c_change = np.sum(np.diff(c_reshape[:,i]))
+        if not c_change:
             mask[i] = False
-    z_reshape = z_reshape[:,mask]
+    c_reshape = c_reshape[:,mask]
 
     c_train, c_test, z_train, z_test = train_test_split(c_reshape, z_reshape, test_size=0.2, shuffle=False)
-    scores = dci._compute_dci(c_train[:8000,:].transpose(), z_train[:8000,:].transpose(), c_test[:2000,:].transpose(), z_test[:2000,:].transpose())
+    scores = dci._compute_dci(z_train[:8000,:].transpose(), c_train[:8000,:].transpose(), z_test[:2000,:].transpose(), c_test[:2000,:].transpose())
 
     print('D: {}'.format(scores['disentanglement']))
     print('C: {}'.format(scores['completeness']))
