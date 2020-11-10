5ddec6005b39c8120439ba01e4fd48c98c2d03e8
simonbing <bings@student.ethz.ch>
2020-11-10 15:39:04 +0100

    new ada baseline training script

--- scripts/ada_subset_pair_new.sh
+++ scripts/ada_subset_pair_new.sh
@@ -3,7 +3,7 @@
 for n in {1..5}; do
   seed=$RANDOM
   bsub -g /disent_baseline -R "rusage[mem=16000,ngpus_excl_p=1]" \
-  python baselines/train_baseline.py --base_dir ada_new_pairing_sin \
+  python baselines/train_baseline.py --base_dir ada_new_pairing_sin100 \
   --output_dir n_"$n" --dim 64 --model adagvae --seed "$seed" \
-  --steps 15620 --subset sin_rand
+  --steps 15620 --subset sin_rand_100
 done
