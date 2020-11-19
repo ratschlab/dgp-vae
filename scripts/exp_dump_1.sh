eb71b4b796c19ca8be6bac9b9f1611b66e172028
simonbing <bings@student.ethz.ch>
2020-11-19 16:12:40 +0100

    quick fix, something still wrong with visualization for gp dataset

--- scripts/exp_dump_1.sh
+++ scripts/exp_dump_1.sh
@@ -16,25 +16,25 @@ for n in {1..5}; do
   seed=$RANDOM
   # Base model
   # Sinusoid data, period resampled
-  bsub -o models/sin_resamp/base/len_5/log_%J -g /gpvae_disent \
-  -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
-  python run_experiment.py --model_type gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
-  --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_sin_resample.npz \
-  --exp_name n_"$n" --basedir models/sin_resamp/base/len_5 \
-  --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
-  --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
-  --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_sin_resample.npz \
-  --score_factors=1,2,3,4,5 --save_score --visualize_score
+#  bsub -o models/sin_resamp/base/len_5/log_%J -g /gpvae_disent \
+#  -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
+#  python run_experiment.py --model_type gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
+#  --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_sin_resample.npz \
+#  --exp_name n_"$n" --basedir models/sin_resamp/base/len_5 \
+#  --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
+#  --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
+#  --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_sin_resample.npz \
+#  --score_factors=1,2,3,4,5 --save_score --visualize_score
 
-  bsub -o models/sin_resamp/base/len_10/log_%J -g /gpvae_disent \
-  -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
-  python run_experiment.py --model_type gp-vae --data_type dsprites --time_len 10 --testing --batch_size 32 \
-  --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_sin_resample.npz \
-  --exp_name n_"$n" --basedir models/sin_resamp/base/len_10 \
-  --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
-  --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
-  --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_sin_resample.npz \
-  --score_factors=1,2,3,4,5 --save_score --visualize_score
+#  bsub -o models/sin_resamp/base/len_10/log_%J -g /gpvae_disent \
+#  -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
+#  python run_experiment.py --model_type gp-vae --data_type dsprites --time_len 10 --testing --batch_size 32 \
+#  --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_sin_resample.npz \
+#  --exp_name n_"$n" --basedir models/sin_resamp/base/len_10 \
+#  --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
+#  --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
+#  --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_sin_resample.npz \
+#  --score_factors=1,2,3,4,5 --save_score --visualize_score
 
   # GP data, full range
   bsub -o models/gp_full/base/len_5/log_%J -g /gpvae_disent \
@@ -81,15 +81,15 @@ for n in {1..5}; do
 
   # Ada extended model
   # Sinusoid data, period resampled
-    bsub -o models/sin_resamp/ada/len_5/log_%J -g /gpvae_disent \
-  -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
-  python run_experiment.py --model_type ada-gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
-  --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_sin_resample.npz \
-  --exp_name n_"$n" --basedir models/sin_resamp/ada/len_5 \
-  --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
-  --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
-  --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_sin_resample.npz \
-  --score_factors=1,2,3,4,5 --save_score --visualize_score
+#    bsub -o models/sin_resamp/ada/len_5/log_%J -g /gpvae_disent \
+#  -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
+#  python run_experiment.py --model_type ada-gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
+#  --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_sin_resample.npz \
+#  --exp_name n_"$n" --basedir models/sin_resamp/ada/len_5 \
+#  --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
+#  --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
+#  --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_sin_resample.npz \
+#  --score_factors=1,2,3,4,5 --save_score --visualize_score
 
   # GP data, full range
   bsub -o models/gp_full/ada/len_5/log_%J -g /gpvae_disent \
