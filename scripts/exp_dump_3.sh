935f07a7894b38a08152088ade3f93149fcb8c63
simonbing <bings@student.ethz.ch>
2020-11-28 11:34:38 +0100

    new gp part range data experiments

--- scripts/exp_dump_3.sh
+++ scripts/exp_dump_3.sh
@@ -62,7 +62,7 @@ for n in {1..5}; do
   -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
   python run_experiment.py --model_type ada-gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
   --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_part_range2.npz \
-  --exp_name n_"$n" --basedir models/gp_full2/ada/len_5/same \
+  --exp_name n_"$n" --basedir models/gp_part2/ada/len_5/same \
   --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
   --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
   --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_part_range2.npz \
@@ -72,7 +72,7 @@ for n in {1..5}; do
   -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
   python run_experiment.py --model_type ada-gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
   --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_part_range2.npz \
-  --exp_name n_"$n" --basedir models/gp_full2/ada/len_5/scaled --len_init scaled --kernel_scales 16 \
+  --exp_name n_"$n" --basedir models/gp_part2/ada/len_5/scaled --len_init scaled --kernel_scales 16 \
   --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
   --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 20 --beta 1.0 \
   --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_part_range2.npz \
