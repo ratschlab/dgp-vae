d93e3f4dde8e0fa307f28bea20734023d4551cee
simonbing <bings@student.ethz.ch>
2020-12-20 09:40:25 +0100

    baselines test script with new datasets

--- scripts/ada_other_data_baseline.sh
+++ scripts/ada_other_data_baseline.sh
@@ -2,11 +2,12 @@
 
 for n in 99; do
   seed=0
-  for subset in norb_full1 cars_full1 shapes_full1 shapes_full2; do
-    bsub -o baselines/adagvae/"$subset"/log_%J \
-    -g /disent_baseline -R "rusage[mem=16000,ngpus_excl_p=1]" \
-    python baselines/train_baseline.py --base_dir ada_"$subset" \
-    --output_dir n_"$n" --dim 64 --model adagvae --seed 0 \
-    --steps 15620 --subset "$subset"
+# for subset in norb_full1 cars_full1 shapes_full1 shapes_full2; do
+  for subset in norb_full1 cars_full1; do
+  bsub -o baselines/adagvae/"$subset"/log_%J \
+  -g /disent_baseline -R "rusage[mem=16000,ngpus_excl_p=1]" \
+  python baselines/train_baseline.py --base_dir ada_"$subset" \
+  --output_dir n_"$n" --dim 64 --model adagvae --seed 0 \
+  --steps 15620 --subset "$subset"
   done
 done
