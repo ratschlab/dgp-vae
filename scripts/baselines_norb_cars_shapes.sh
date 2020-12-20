6d09bd0b279916eac279228942415a27829bbbce
simonbing <bings@student.ethz.ch>
2020-12-20 16:49:14 +0100

    Updated baselines experiment script

--- scripts/baselines_norb_cars_shapes.sh
+++ scripts/baselines_norb_cars_shapes.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-for n in {1..5}; do
+for n in {1..10}; do
   seed=$RANDOM
   for model in adagvae annealedvae betavae betatcvae factorvae dipvae_i dipvae_ii; do
-    for subset in norb_full1 cars_full1 shapes_full1 shapes_full2; do
+    for i in smallnorb,norb_full1 cars3d,cars_full1 shapes3d,shapes_full1 shapes3d,shapes_full2; do
+      IFS=',' read data subset <<< "${i}"
       bsub -o baselines/adagvae/"$subset"/log_%J \
       -g /disent_baseline -R "rusage[mem=16000,ngpus_excl_p=1]" \
       python baselines/train_baseline.py --base_dir "$subset" \
       --output_dir n_"$n" --dim 64 --model "$model" --seed "$seed" \
-      --steps 15620 --subset "$subset"
+      --steps 15620 --data "$data" --subset "$subset"
     done
   done
 done
