7b366d32a1e4781e959c8a5568b07bca895f1304
simonbing <bings@student.ethz.ch>
2020-12-31 10:45:42 +0100

    new cars baseline experiments

--- scripts/baselines_norb_cars_shapes.sh
+++ scripts/baselines_norb_cars_shapes.sh
@@ -4,7 +4,7 @@ for n in {1..10}; do
   seed=$RANDOM
   for model in adagvae annealedvae betavae betatcvae factorvae dipvae_i dipvae_ii; do
 #    for i in smallnorb,norb_full1 cars3d,cars_full1 shapes3d,shapes_full1 shapes3d,shapes_full2; do
-    for i in cars3d,cars_full1 shapes3d,shapes_full1 shapes3d,shapes_full2; do
+    for i in cars3d,cars_part1 cars3d,cars_full4; do
       IFS=',' read data subset <<< "${i}"
       bsub -o baselines/adagvae/"$subset"/log_%J \
       -g /disent_baseline -R "rusage[mem=60000,ngpus_excl_p=1]" \
