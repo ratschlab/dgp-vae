9af20a9a3158c16d6b730984dc6ffebe87326508
simonbing <bings@student.ethz.ch>
2021-01-05 23:18:39 +0100

    updated baselines exp and dsprites exp script

--- scripts/baselines.sh
+++ scripts/baselines.sh
@@ -4,7 +4,7 @@ for n in {1..10}; do
   seed=$RANDOM
   for model in adagvae annealedvae betavae betatcvae factorvae dipvae_i dipvae_ii; do
 #    for i in smallnorb,norb_full1 cars3d,cars_full1 shapes3d,shapes_full1 shapes3d,shapes_full2; do
-    for i in dsprites_full,gp_full_3 dsprites_full,gp_part_3 dsprites_full,gp_full_4 dsprites_full,gp_part_4; do
+    for i in dsprites_full,gp_full_4; do
       IFS=',' read data subset <<< "${i}"
       bsub -o baselines/adagvae/"$subset"/log_%J \
       -g /disent_baseline -R "rusage[mem=60000,ngpus_excl_p=1]" \
--- scripts/exp_dump_5.sh
+++ scripts/exp_dump_5.sh
@@ -46,58 +46,58 @@ for n in {6..10}; do
       --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
       --score_factors=1,2,3,4,5 --save_score --visualize_score
 
-      bsub -o models/gp_"$type""$count"/base/len_5/scaled/log_%J -g /gpvae_disent \
-      -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
-      python run_experiment.py --model_type gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
-      --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_"$type"_range"$count".npz \
-      --exp_name n_"$n" --basedir models/gp_"$type""$count"/base/len_5/scaled --len_init scaled --kernel_scales 16 \
-      --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
-      --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 20 --beta 1.0 \
-      --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
-      --score_factors=1,2,3,4,5 --save_score --visualize_score
-
-      bsub -o models/gp_"$type""$count"/base/len_10/same/log_%J -g /gpvae_disent \
-      -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
-      python run_experiment.py --model_type gp-vae --data_type dsprites --time_len 10 --testing --batch_size 32 \
-      --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_"$type"_range"$count".npz \
-      --exp_name n_"$n" --basedir models/gp_"$type""$count"/base/len_10/same \
-      --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
-      --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
-      --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
-      --score_factors=1,2,3,4,5 --save_score --visualize_score
-
-      bsub -o models/gp_"$type""$count"/base/len_10/scaled/log_%J -g /gpvae_disent \
-      -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
-      python run_experiment.py --model_type gp-vae --data_type dsprites --time_len 10 --testing --batch_size 32 \
-      --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_"$type"_range"$count".npz \
-      --exp_name n_"$n" --basedir models/gp_"$type""$count"/base/len_10/scaled --len_init scaled --kernel_scales 16 \
-      --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
-      --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 20 --beta 1.0 \
-      --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
-      --score_factors=1,2,3,4,5 --save_score --visualize_score
-
-
-      # Ada extended model
-      # GP data, part range
-      bsub -o models/gp_"$type""$count"/ada/len_5/same/log_%J -g /gpvae_disent \
-      -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
-      python run_experiment.py --model_type ada-gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
-      --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_"$type"_range"$count".npz \
-      --exp_name n_"$n" --basedir models/gp_"$type""$count"/ada/len_5/same \
-      --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
-      --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
-      --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
-      --score_factors=1,2,3,4,5 --save_score --visualize_score
-
-      bsub -o models/gp_"$type""$count"/ada/len_5/scaled/log_%J -g /gpvae_disent \
-      -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
-      python run_experiment.py --model_type ada-gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
-      --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_"$type"_range"$count".npz \
-      --exp_name n_"$n" --basedir models/gp_"$type""$count"/ada/len_5/scaled --len_init scaled --kernel_scales 16 \
-      --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
-      --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 20 --beta 1.0 \
-      --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
-      --score_factors=1,2,3,4,5 --save_score --visualize_score
+#      bsub -o models/gp_"$type""$count"/base/len_5/scaled/log_%J -g /gpvae_disent \
+#      -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
+#      python run_experiment.py --model_type gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
+#      --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_"$type"_range"$count".npz \
+#      --exp_name n_"$n" --basedir models/gp_"$type""$count"/base/len_5/scaled --len_init scaled --kernel_scales 16 \
+#      --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
+#      --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 20 --beta 1.0 \
+#      --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
+#      --score_factors=1,2,3,4,5 --save_score --visualize_score
+#
+#      bsub -o models/gp_"$type""$count"/base/len_10/same/log_%J -g /gpvae_disent \
+#      -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
+#      python run_experiment.py --model_type gp-vae --data_type dsprites --time_len 10 --testing --batch_size 32 \
+#      --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_"$type"_range"$count".npz \
+#      --exp_name n_"$n" --basedir models/gp_"$type""$count"/base/len_10/same \
+#      --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
+#      --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
+#      --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
+#      --score_factors=1,2,3,4,5 --save_score --visualize_score
+#
+#      bsub -o models/gp_"$type""$count"/base/len_10/scaled/log_%J -g /gpvae_disent \
+#      -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
+#      python run_experiment.py --model_type gp-vae --data_type dsprites --time_len 10 --testing --batch_size 32 \
+#      --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_"$type"_range"$count".npz \
+#      --exp_name n_"$n" --basedir models/gp_"$type""$count"/base/len_10/scaled --len_init scaled --kernel_scales 16 \
+#      --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
+#      --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 20 --beta 1.0 \
+#      --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
+#      --score_factors=1,2,3,4,5 --save_score --visualize_score
+#
+#
+#      # Ada extended model
+#      # GP data, part range
+#      bsub -o models/gp_"$type""$count"/ada/len_5/same/log_%J -g /gpvae_disent \
+#      -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
+#      python run_experiment.py --model_type ada-gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
+#      --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_"$type"_range"$count".npz \
+#      --exp_name n_"$n" --basedir models/gp_"$type""$count"/ada/len_5/same \
+#      --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
+#      --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
+#      --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
+#      --score_factors=1,2,3,4,5 --save_score --visualize_score
+#
+#      bsub -o models/gp_"$type""$count"/ada/len_5/scaled/log_%J -g /gpvae_disent \
+#      -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
+#      python run_experiment.py --model_type ada-gp-vae --data_type dsprites --time_len 5 --testing --batch_size 32 \
+#      --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/dsprites_gp_"$type"_range"$count".npz \
+#      --exp_name n_"$n" --basedir models/gp_"$type""$count"/ada/len_5/scaled --len_init scaled --kernel_scales 16 \
+#      --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
+#      --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 20 --beta 1.0 \
+#      --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_"$type"_range"$count".npz \
+#      --score_factors=1,2,3,4,5 --save_score --visualize_score
     done
   done
 done
