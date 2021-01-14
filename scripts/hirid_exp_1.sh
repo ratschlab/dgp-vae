75c571c097a023d5bcd0d51648659273767eac63
simonbing <bings@student.ethz.ch>
2021-01-14 12:02:46 +0100

    hirid exp script

--- scripts/hirid_exp_1.sh
+++ scripts/hirid_exp_1.sh
@@ -52,11 +52,11 @@ for n in {1..5}; do
         -g /gpvae_disent -W "$run_time":00 -R "rusage[mem=65000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
         python run_experiment.py --model_type gp-vae --data_type hirid --time_len "$time_len" \
         --testing --batch_size 64 --exp_name n_"$n" --basedir models/hirid/"$sens_eval_type"/dim_"$lat_dim"/len_"$time_len"/same \
-        --len_init same --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_std.npz \
+        --len_init same --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_"$sens_eval_type".npz \
         --seed "$seed" --banded_covar --latent_dim "$lat_dim" --encoder_sizes=128,128 \
         --decoder_sizes=256,256 --window_size "$window_size" --sigma 1.005 --length_scale 2.0 --beta 1.0 \
-        --num_epochs 1 --kernel cauchy --eval_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_sensitivity_eval_std.npz \
-        --visualize --save --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_std.npz \
+        --num_epochs 1 --kernel cauchy --eval_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_sensitivity_eval_"$sens_eval_type".npz \
+        --visualize --save --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_"$sens_eval_type".npz \
         --visualize_score --save_score --eval_type both --sens_eval_type "$sens_eval_type" \
         --data_type_dci hirid --shuffle
 
@@ -64,11 +64,11 @@ for n in {1..5}; do
         -g /gpvae_disent -W "$run_time":00 -R "rusage[mem=65000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
         python run_experiment.py --model_type gp-vae --data_type hirid --time_len "$time_len" \
         --testing --batch_size 64 --exp_name n_"$n" --basedir models/hirid/"$sens_eval_type"/dim_"$lat_dim"/len_"$time_len"/scaled \
-        --len_init scaled --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_std.npz \
+        --len_init scaled --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_"$sens_eval_type".npz \
         --seed "$seed" --banded_covar --latent_dim "$lat_dim" --encoder_sizes=128,128 --kernel_scales "$latent_dim"\
         --decoder_sizes=256,256 --window_size "$window_size" --sigma 1.005 --length_scale 20.0 --beta 1.0 \
-        --num_epochs 1 --kernel cauchy --eval_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_sensitivity_eval_std.npz \
-        --visualize --save --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_std.npz \
+        --num_epochs 1 --kernel cauchy --eval_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_sensitivity_eval_"$sens_eval_type".npz \
+        --visualize --save --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_"$sens_eval_type".npz \
         --visualize_score --save_score --eval_type both --sens_eval_type "$sens_eval_type" \
         --data_type_dci hirid --shuffle
 
