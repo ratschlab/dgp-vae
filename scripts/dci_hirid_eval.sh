afcc609887c12e9fd0aba9f37a04fc72e4764c46
simonbing <bings@student.ethz.ch>
2021-01-19 17:27:14 +0100

    dci reeval of best hirid model with different seeds

--- scripts/dci_hirid_eval.sh
+++ scripts/dci_hirid_eval.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 for n in {1..100}; do
-  bsub -g /gpvae_norm -R "rusage[mem=65000]" python dsprites_dci.py \
+  bsub -o /cluster/work/grlab/projects/projects2020_disentangled_gpvae/models/hirid/std/dim_8/len_50/scaled/log_%J \
+  -g /gpvae_norm -R "rusage[mem=65000]" python dsprites_dci.py \
   --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_std.npz \
   --model_name /cluster/work/grlab/projects/projects2020_disentangled_gpvae/models/hirid/std/dim_8/len_50/scaled/210114_n_3 --save_score \
   --dci_seed "$RANDOM"
