61bddfcc3ec859c146ebc5725d0bfa3152cb1cbf
simonbing <bings@student.ethz.ch>
2021-01-22 13:31:38 +0100

    hirid ada comp experiments without banded covariance

--- scripts/hirid_comp_exp.sh
+++ scripts/hirid_comp_exp.sh
@@ -49,10 +49,10 @@ for n_seed in 1,22019 2,3412 3,13351 4,2677 5,6071 6,10843 7,16040 8,8088 9,2206
 #    done
     bsub -o models/hirid/comp/ada/dim_"$lat_dim"/log_%J -g /gpvae_norm -W 62:00 -R "rusage[mem=65000,ngpus_excl_p=1]" \
     python run_experiment.py --model_type ada-gp-vae \
-    --data_type hirid --time_len 1 --testing --batch_size 64 --exp_name n_"$n"_band \
+    --data_type hirid --time_len 1 --testing --batch_size 64 --exp_name n_"$n"_noband \
     --basedir models/hirid/comp/ada/dim_"$lat_dim" --len_init same \
     --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_std.npz \
-    --seed "$seed" --banded_covar --latent_dim "$lat_dim" --encoder_sizes=128,128 --decoder_sizes=256,256 \
+    --seed "$seed" --latent_dim "$lat_dim" --encoder_sizes=128,128 --decoder_sizes=256,256 \
     --window_size 1 --sigma 1.005 --beta 1.0 --num_steps 404683 --kernel id \
     --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_std.npz \
     --visualize_score --save_score --eval_type dci --data_type_dci hirid --shuffle --dci_seed "$seed"
