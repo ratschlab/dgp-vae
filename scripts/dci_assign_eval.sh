a8fcc2c4cb8ed90e8f68e02fb7d2619080ded5b2
simonbing <bings@student.ethz.ch>
2021-01-23 15:29:10 +0100

    dci eval with 10843 seed

--- scripts/dci_assign_eval.sh
+++ scripts/dci_assign_eval.sh
@@ -77,7 +77,7 @@ for model_seed in 210121_n_1,22019 210121_n_2,3412 210121_n_3,13351 210121_n_4,2
   --assign_mat_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/assign_mats/assign_mat_1.npy \
   --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/hirid_std.npz \
   --model_name /cluster/work/grlab/projects/projects2020_disentangled_gpvae/models/hirid/comp1/base/dim_16/len_10/scaled/"$model" \
-  --data_type_dci hirid --save_score --visualize_score --shuffle --dci_seed 18112
+  --data_type_dci hirid --save_score --visualize_score --shuffle --dci_seed 10843
 done
 
 #for model_seed in 210120_n_1,22019 210121_n_2,3412 210121_n_3,13351 210121_n_4,2677 210121_n_5,6071 210121_n_6,10843 210121_n_7,16040 210121_n_8,8088 210121_n_9,22060 210121_n_10,29153; do
