9a1d9acd452940d9a5a9033bb75e96fa2b60609d
simonbing <bings@student.ethz.ch>
2021-01-23 13:37:12 +0100

    updating aggregate script for hirid data

--- dci_aggregate.py
+++ dci_aggregate.py
@@ -119,7 +119,7 @@ def main(argv):
         dci_scores = aggregate_baseline(FLAGS.n, FLAGS.params, FLAGS.base_dir)
     elif FLAGS.model == 'hirid':
         print('Hirid')
-        dci_scores = aggregate_hirid(FLAGS.n, FLAGS.base_dir)
+        dci_scores = aggregate_hirid(FLAGS.n)
     else:
         raise ValueError("Model must be one of: ['gpvae', 'annealedvae', 'betavae', 'betatcvae', 'factorvae', 'dipvae_i', 'dipvae_ii']")
 
