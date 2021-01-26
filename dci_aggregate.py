20268306e24df929977b2e1ea2638dc777eebe9b
simonbing <bings@student.ethz.ch>
2021-01-26 13:35:21 +0100

    refactor aggregation script

--- dci_aggregate.py
+++ dci_aggregate.py
@@ -48,14 +48,11 @@ def aggregate_gpvae(N, base_dir):
 
     for i, subdir in enumerate(subdirs):
         potential_paths = [file.name for file in os.scandir(subdir) if file.name.endswith('npz')]
-        print(potential_paths)
         single_score_path = os.path.join(subdir,potential_paths[0])
         single_score = np.load(single_score_path)
-        print(single_score.files)
-        # dci_scores[i, 0] = single_score['disentanglement']
-        # dci_scores[i, 1] = single_score['completeness']
-        # dci_scores[i, 2] = single_score['disentanglement_assign']
-        # dci_scores[i, 3] = single_score['completeness_assign']
+        dci_scores[i, 0] = single_score['disentanglement']
+        dci_scores[i, 1] = single_score['completeness']
+        dci_scores[i, 2] = single_score['informativeness_test']
 
     # dci_scores = np.zeros((3,N,len(params)), dtype=np.float32)
     # print(len(params))
@@ -158,16 +155,16 @@ def main(argv):
     std_scores = np.std(dci_scores, axis=0)
     print(F'Mean D: {np.round(mean_scores[0], 3)}')
     print(F'Mean C: {np.round(mean_scores[1], 3)}')
-    print(F'Mean D assign: {np.round(mean_scores[2], 3)}')
-    print(F'Std D assign: {np.round(std_scores[2], 3)}')
-    print(F'Mean C assign: {np.round(mean_scores[3], 3)}')
-    print(F'Std C assign: {np.round(std_scores[3], 3)}')
+    # print(F'Mean D assign: {np.round(mean_scores[2], 3)}')
+    # print(F'Std D assign: {np.round(std_scores[2], 3)}')
+    # print(F'Mean C assign: {np.round(mean_scores[3], 3)}')
+    # print(F'Std C assign: {np.round(std_scores[3], 3)}')
     # print(F"Mean: {np.round(np.mean(dci_scores[0,...]), 2)}")
     # print(F"Median: {np.round(np.median(dci_scores[0,...]), 2)}")
 
     if FLAGS.save:
         if FLAGS.model == 'gpvae':
-            np.save(os.path.join('models', FLAGS.exp_name, 'dci_aggr.npy'), dci_scores)
+            np.save(os.path.join(FLAGS.base_dir, 'dci_aggr.npy'), dci_scores)
             print(F"Saved scores at :{os.path.join('models', FLAGS.exp_name, 'dci_aggr.npy')}")
         elif FLAGS.model in ['adagvae', 'annealedvae', 'betavae', 'betatcvae', 'factorvae', 'dipvae_i', 'dipvae_ii']:
             np.save(os.path.join('baselines', FLAGS.model, FLAGS.exp_name, F'{FLAGS.model}_{FLAGS.exp_name}_dci.npy'), dci_scores)
