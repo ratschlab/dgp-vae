f520d711c7c9f835b1b23cb351d2f5778446ade9
simonbing <bings@student.ethz.ch>
2021-01-27 15:57:22 +0100

    Missing seeds experiments for norb and cars dataset

--- scripts/dgpvae_missing_seeds.sh
+++ scripts/dgpvae_missing_seeds.sh
@@ -15,7 +15,7 @@ for n in {21..25}; do
   bsub -o /cluster/work/grlab/projects/projects2020_disentangled_gpvae/models/cars_part1/base/len_5/same/log_%J -g /gpvae_disent \
   -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
   python run_experiment.py --model_type gp-vae --data_type cars3d --time_len 5 --testing --batch_size 32 \
-  --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/car3d/cars_part1.npz \
+  --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/cars3d/cars_part1.npz \
   --exp_name n_"$n" --basedir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/models/cars_part1/base/len_5/same \
   --seed "$seed" --banded_covar --latent_dim 64 --encoder_sizes=32,256,256 \
   --decoder_sizes=256,256,256 --window_size 3 --sigma 1 --length_scale 2 --beta 1.0 \
