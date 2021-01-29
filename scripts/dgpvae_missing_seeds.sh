5b91e241fd391f12718d169d30f178d165862828
simonbing <bings@student.ethz.ch>
2021-01-29 18:06:43 +0100

    missing cars experiments

--- scripts/dgpvae_missing_seeds.sh
+++ scripts/dgpvae_missing_seeds.sh
@@ -12,8 +12,8 @@ for n in {21..25}; do
 #  --num_epochs 1 --kernel cauchy --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/norb/factors_norb_full1.npz \
 #  --save_score
 
-  bsub -o log_%J -g /gpvae_disent \
-  -R "rusage[mem=150000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
+  bsub -o cars_log_%J -g /gpvae_disent \
+  -R "rusage[mem=200000,ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" \
   python run_experiment.py --model_type gp-vae --data_type cars3d --time_len 5 --testing --batch_size 32 \
   --data_dir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/cars3d/cars_part1.npz \
   --exp_name n_"$n" --basedir /cluster/work/grlab/projects/projects2020_disentangled_gpvae/models/cars_part1/base/len_5/same \
