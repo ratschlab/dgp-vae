fe9ef2c998411ab47a7c5c091debc40891a029ab
simonbing <bings@student.ethz.ch>
2021-01-29 11:21:54 +0100

    Initial commit for classifier

---
+++ classifier.py
@@ -0,0 +1,27 @@
+"""
+Classifier for downstream proxy task of hirid representations.
+"""
+
+import numpy as np
+import os
+from absl import flags, app
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer('seed', 42, 'Random seed')
+flags.DEFINE_string('labels_path', '/cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/hirid/mort_labels_test.npy', 'Hirid classification labels')
+flags.DEFINE_string('representation_path', '/cluster/home/bings/dgpvae/models/hirid/comp/base/dim_8/len_25/scaled/n_scales_4/210126_n_1', 'Path to latent representation that are used as input features.')
+
+def main(argv):
+    del argv # Unused
+
+    # Load labels and representations
+    labels_full = np.load(FLAGS.labels_path)
+    reps_path = os.path.join(FLAGS.representation_path, 'z_mean.npy')
+    reps_full = np.load(reps_path)
+
+    print(labels_full.shape)
+    print(reps_full.shape)
+
+if __name__ == '__main__':
+    app.run(main)
