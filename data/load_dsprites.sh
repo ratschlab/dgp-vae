2bbc1045b0c4bfcd3d859c2577da94e2897c25e7
simonbing <bings@student.ethz.ch>
2021-02-02 19:00:08 +0100

    Adds data loading scripts

--- data/load_dsprites.sh
+++ data/load_dsprites.sh
@@ -1,6 +1,4 @@
 DATA_DIR="data/dsprites"
 
 mkdir -p ${DATA_DIR}
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1RxWbKPNST6oWQ1LGPCr7aSpSQrtkKzZU' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1RxWbKPNST6oWQ1LGPCr7aSpSQrtkKzZU" -O ${DATA_DIR}/dsprites_5000.npz && rm -rf /tmp/cookies.txt
-
-# WARNING LINK DOES NOT WORK YET
+wget https://www.dropbox.com/s/0er8mboiw4rmw7s/factors_dsprites.npz?dl=0 -O ${DATA_DIR}/factors_dsprites.npz
