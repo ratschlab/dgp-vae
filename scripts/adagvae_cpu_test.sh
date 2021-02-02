1a486eba169b9b1502dc75a783f4034026dfb847
simonbing <bings@student.ethz.ch>
2021-02-02 19:05:24 +0100

    Remove cluster training scripts.

--- scripts/adagvae_cpu_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for n in 1,2,3; do
-  python baselines/train_baseline.py --base_dir test_dump --output_dir 300k_test_"$n" \
-  --dim 64 --model adagvae --seed "$RANDOM" --steps 300000
-done
--- scripts/hirid_perm_slices.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for n in {0..258}; do
-  bsub -g /gpvae_eval -R "rusage[mem=10000]" python hirid_labels.py -n "$n"
-done
